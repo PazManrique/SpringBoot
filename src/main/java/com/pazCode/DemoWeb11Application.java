fda84bf6a91e359b50c3bd3e5bf2a08418d19f41
Manrique <maria-paz.manrique@capgemini.com>
2022-12-13 13:07:30 +0100

    my first commit

---
+++ src/main/java/com/pazCode/DemoWeb11Application.java
@@ -0,0 +1,13 @@
+package com.pazCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWeb11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWeb11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pazCode/DemoWeb11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pazCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWeb11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
